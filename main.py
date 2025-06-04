436e2ff9886322ccb8a42395256a38049ce3679b
Юлия (Джулия) <sixquestions666@gmail.com>
2025-06-04 03:30:03 +0300

    add class Bird

---
+++ main.py
@@ -0,0 +1,19 @@
+class  Animal:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def make_sound(self):
+        print(f"{self.name} издает звук")
+
+    def eat(self):
+        print(f"{self.name} ест")
+
+class Bird(Animal):
+    def __init__(self, name, age, color):
+        super().__init__(name, age)
+        self.color = color
+
+    def make_sound(self):
+        print(f"{self.name} чирикает")
+
